a00f0c45b05432b33db3773d2464ddcc61f01a9c
LABCAT <shane.watters.nz@gmail.com>
2021-11-30 18:24:47 +1300

    Completing sketch and preparing screenshots.

--- src/js/P5SketchWithAudio.js
+++ src/js/P5SketchWithAudio.js
@@ -30,13 +30,14 @@ const P5SketchWithAudio = () => {
         p.loadMidi = () => {
             Midi.fromUrl(midi).then(
                 function(result) {
-                    console.log(result);
                     const noteSet1 = result.tracks[3].notes; // Synth 2 - Init Patch
                     const noteSet2 = result.tracks[4].notes; // Synth 3 - Bass Guitar
                     const noteSet3 = result.tracks[2].notes; // Synth 4 - Solina Strings
+                    const noteSet4 = result.tracks[6].notes.filter(note => note.midi === 36 || note.midi === 37); // Redrum 1
                     p.scheduleCueSet(noteSet1, 'executeCueSet1');
                     p.scheduleCueSet(noteSet2, 'executeCueSet2');
                     p.scheduleCueSet(noteSet3, 'executeCueSet3');
+                    p.scheduleCueSet(noteSet4, 'executeCueSet4');
                     p.audioLoaded = true;
                     document.getElementById("loader").classList.add("loading--complete");
                     document.getElementById("play-icon").classList.remove("fade-out");
@@ -69,7 +70,7 @@ const P5SketchWithAudio = () => {
             p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
             p.colorMode(p.HSB, 360, 100, 100);
             p.stroke(255);
-            p.strokeWeight(2);
+            p.noFill();
             p.background(0);
             p.noLoop();
             p.maxSpiralSize = p.width >= p.height ? p.height / 2 : p.width /2;
@@ -82,9 +83,11 @@ const P5SketchWithAudio = () => {
         }
 
         p.executeCueSet1 = (note) => {
-            p.background(0);
+            if(note.currentCue < 90){
+                p.background(0);
+            }
             const sizeMultiplier  = p.random(0.1, 5);
-            p.drawSpiral(p.width/8 * 7, p.height/4 * 3, sizeMultiplier, 1, 1);
+            p.drawSpiral(p.width/8 * 7, p.height/4 * 3, sizeMultiplier, 3, 1);
         }
 
         p.executeCueSet2 = (note) => {
@@ -96,9 +99,24 @@ const P5SketchWithAudio = () => {
         p.executeCueSet3 = (note) => {
             const sizeMultiplier  = p.random(4, 8),
                 spiralWidth = p.random(5, 10),
-                angle = p.random(8, 23);
-            p.drawSpiral(p.width/8 * 5, p.height/4, sizeMultiplier, spiralWidth, angle, 50);
+                angle = p.random(8, 23),
+                size = p.maxSpiralSize * (sizeMultiplier / spiralWidth / angle),
+                delay = note.duration * 1000 / size;
+            p.drawSpiral(p.width/8 * 5, p.height/8 * 3, sizeMultiplier, spiralWidth, angle, delay);
         }
+
+        p.executeCueSet4 = (note) => {
+            const { midi } = note,
+                sizeMultiplier  = p.random(1, 2);
+            if(midi === 36){
+                p.drawSpiral(p.width/2, p.height/8 * 7, sizeMultiplier, 3, 1, 1);
+            }
+            if(midi === 37){
+                p.drawSpiral(p.width/16 * 13, p.height/8 * 1.5, sizeMultiplier, 3, 1, 1);
+            }
+        }
+
+        p.timeouts = [];
 
         p.drawSpiral = (startX, startY, sizeMultiplier = 1, spiralWidth = 20, angle = 1, delayAmount = 10) => {
             const size = p.maxSpiralSize * (sizeMultiplier / spiralWidth / angle),
@@ -112,32 +130,25 @@ const P5SketchWithAudio = () => {
             }
 
             for (let i = 0; i < size; i++) {
-                setTimeout(
-                    function () {
-                        let newAngle = (angle/10) * i;
-                        let x = (startX) + (spiralWidth * newAngle) * Math.sin(newAngle);
-                        let  y = (startY) + (spiralWidth * newAngle) * Math.cos(newAngle);
-                        // p.stroke(p.randomColor()); // Random Color for each line segment
-                        // p.strokeWeight(p.randomWeight()); // Random Weight (1-5)
-                        p.stroke(randomHue, 100 / size * i, 100); 
-                        p.line(oldX, oldY, x, y);
-                        oldX = x;
-                        oldY = y;
-                    },
-                    (delayAmount * i)
+                p.timeouts.push(
+                    setTimeout(
+                        function () {
+                            let newAngle = (angle/10) * i;
+                            let x = (startX) + (spiralWidth * newAngle) * Math.sin(newAngle);
+                            let  y = (startY) + (spiralWidth * newAngle) * Math.cos(newAngle);
+                            p.strokeWeight(12);
+                            p.stroke(randomHue, 100 / size * i, 100, 0.15); 
+                            p.line(oldX, oldY, x, y);
+                            p.strokeWeight(2);
+                            p.stroke(randomHue, 100 / size * i, 100); 
+                            p.line(oldX, oldY, x, y);
+                            oldX = x;
+                            oldY = y;
+                        },
+                        (delayAmount * i)
+                    )
                 );
             }
-        }
-
-        p.randomColor = () => {
-            const r = Math.round(Math.random() * 255);
-            const g = Math.round(Math.random() * 255);
-            const b = Math.round(Math.random() * 255);
-            return `rgb(${r}, ${g}, ${b})`;
-        }
-
-        p.randomWeight = () => {
-            return Math.random() * 5;
         }
 
         p.mousePressed = () => {
@@ -161,14 +172,17 @@ const P5SketchWithAudio = () => {
         p.logCredits = () => {
             if (
                 !p.creditsLogged &&
-                parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
+                Math.ceil(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
             ) {
                 p.creditsLogged = true;
-                    console.log(
+                console.log(
                     "Music By: http://labcat.nz/",
                     "\n",
                     "Animation By: https://github.com/LABCAT/"
                 );
+                for (let i = 0; i < p.timeouts.length; i++) {
+                    clearTimeout(p.timeouts[i]);
+                }
                 p.song.stop();
             }
         };
